ec6ed279ff00076ff34da4002698422f1cf1355d
Vaibhavgetch <vbhv.sxn2518@gmail.com>
2023-12-07 13:52:51 +0530

    Added Preorder Iterative Tree traveral solution

---
+++ src/Trees/BinaryTrees/Traversal/IterativePreorder.java
@@ -0,0 +1,32 @@
+package Trees.BinaryTrees.Traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class IterativePreorder {
+
+    public List<Integer> preOrderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> stk = new Stack<>();
+
+        if (root == null) return new ArrayList<>();
+        stk.push(root);
+
+        while (stk.size() > 0) {
+            //Remove
+            root = stk.pop();
+            ans.add(root.val);
+
+            // check if right child is present, ifPresent() --> add to stack
+            // we are adding right first to preserve stack property
+            if (root.right != null) {
+                stk.push(root.right);
+            }
+            if (root.left != null) {
+                stk.push(root.left);
+            }
+        }
+        return ans;
+    }
+}
