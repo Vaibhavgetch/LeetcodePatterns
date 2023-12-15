f6e2a0e8da8b3ab5d5ef97f3262a37b64e4050af
Vaibhavgetch <vbhv.sxn2518@gmail.com>
2023-12-15 12:36:42 +0530

    Added Trees Medium problem

---
+++ src/Trees/BinaryTrees/Medium/BinaryTreeZigZagLevel.java
@@ -0,0 +1,46 @@
+package Trees.BinaryTrees.Medium;
+
+
+import Trees.BinaryTrees.Traversal.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/*
+*   https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+* */
+public class BinaryTreeZigZagLevel {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if(root == null) return new ArrayList<>() ;
+
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        List<List<Integer>> result = new ArrayList<>();
+         boolean reversed = false ;
+         dq.addFirst(root);
+
+         while(dq.size() > 0){
+             int count = dq.size();
+             List<Integer> temp = new ArrayList<>();
+             for(int i = 0 ; i < count ; i++){
+                 if(!reversed){
+                    TreeNode curr = dq.removeFirst();
+                    temp.add(curr.val);
+                    if(curr.left != null) dq.addLast(curr.left);
+                    if(curr.right != null) dq.addLast(curr.right);
+                 }
+                 else{
+                     TreeNode curr = dq.removeLast();
+                     temp.add(curr.val);
+                     if(curr.left != null) dq.addFirst(curr.left);
+                     if(curr.right != null) dq.addFirst(curr.right);
+                 }
+                 result.add(temp);
+                 reversed = !reversed;
+             }
+         }
+         return result ;
+    }
+}
--- src/Trees/BinaryTrees/Traversal/TreeNode.java
+++ src/Trees/BinaryTrees/Traversal/TreeNode.java
@@ -3,10 +3,10 @@ package Trees.BinaryTrees.Traversal;
 /*
   Definition for a binary tree node.
 */
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
     TreeNode() {
     }
