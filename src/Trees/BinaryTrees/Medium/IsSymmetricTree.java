f116e1efbf61c3e043f594b59dcbcc8d816cf4af
Vaibhavgetch <vbhv.sxn2518@gmail.com>
2023-12-19 13:23:28 +0530

    Added question link

--- src/Trees/BinaryTrees/Medium/IsSymmetricTree.java
+++ src/Trees/BinaryTrees/Medium/IsSymmetricTree.java
@@ -5,6 +5,9 @@ import Trees.BinaryTrees.Traversal.TreeNode;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+ * https://leetcode.com/problems/symmetric-tree/description/
+ * */
 public class IsSymmetricTree {
 
     /*
@@ -13,6 +16,7 @@ public class IsSymmetricTree {
     public boolean isSymmetricDFS(TreeNode root) {
         return root == null || isMirror(root.left, root.right);
     }
+
     public boolean isMirror(TreeNode left, TreeNode right) {
         if (left == null || right == null) {
             return left == right;
@@ -27,27 +31,27 @@ public class IsSymmetricTree {
 
 
     /*
-    *   Using Queue, levelOrder Traversal
-    * */
+     *   Using Queue, levelOrder Traversal
+     * */
     public boolean isSymmetricBFS(TreeNode root) {
 
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root.left);
         q.add(root.right);
-        while(!q.isEmpty()){
-            TreeNode left  = q.poll();
+        while (!q.isEmpty()) {
+            TreeNode left = q.poll();
             TreeNode right = q.poll();
-            if(left==null && right == null)   continue ;
-            if(left==null || right == null) return false ;
-            if(left.val != right.val)
-                return false ;
+            if (left == null && right == null) continue;
+            if (left == null || right == null) return false;
+            if (left.val != right.val)
+                return false;
 
             q.add(left.left);
             q.add(right.right);
             q.add(left.right);
             q.add(right.left);
         }
-        return true ;
+        return true;
     }
 }
 
